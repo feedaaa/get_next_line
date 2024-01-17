926118f7836372aa3c5ccd9fba0c2c63ca597f43
tyyggrr05 <109667869+feedaaa@users.noreply.github.com>
2024-01-17 17:30:29 +0400

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: ffidha <ffidha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 12:27:55 by ffidha            #+#    #+#             */
-/*   Updated: 2023/12/16 10:29:30 by ffidha           ###   ########.fr       */
+/*   Updated: 2024/01/16 12:14:33 by ffidha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,19 @@ size_t	ft_strlen(const char *s)
 	size_t	i;
 
 	i = 0;
-	while (s[i])
+	while (s && s[i])
 		i++;
 	return (i);
 }
 
 char	*ft_strchr(const char *s, int c)
 {
-	int	i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] == (char )c)
-			return ((char *)s + i);
-		i++;
-	}
-	if (s[i] == (char )c)
-		return ((char *) s + i);
-	return (NULL);
+	if (!s)
+		return (0);
+	while (*s != (char)c)
+		if (!*s++)
+			return (0);
+	return ((char *)s);
 }
 
 char	*ft_strjoin(char const *s1, char const *s2)
@@ -46,16 +40,15 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
-
 	all = (char *)malloc(sizeof(char) *(ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!all)
 		return (NULL);
-	while (s1[i])
+	while (s1 && s1[i])
 	{
 		all[i] = s1[i];
 		i++;
 	}
-	while (s2[j])
+	while (s2 && s2[j])
 	{
 		all[i] = s2[j];
 		j++;
