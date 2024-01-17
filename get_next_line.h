55ae495d25c788899d084b77b800c89eb91c240c
tyyggrr05 <109667869+feedaaa@users.noreply.github.com>
2024-01-17 17:30:54 +0400

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -10,21 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-// # define BUFFER_SIZE 4
-
-# include <stdio.h>
-# include <unistd.h>
 # include <fcntl.h>
+# include <limits.h>
+# include <stdio.h>
 # include <stdlib.h>
+# include <unistd.h>
 
-//utils
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+// utils
+
+size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlen(const char *s);
 
-//main function
-char *get_next_line(int fd);
-# endif
+// main function
+
+char	*get_next_line(int fd);
+#endif
